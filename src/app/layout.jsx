6995dd97720ef9c36a8be1bad378a3326bcdf324
Mirabelle Brown <163205944+mirabellebrown@users.noreply.github.com>
2026-05-11 18:36:00 -0700

    Add Vercel Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
   title: 'Prereqly | UCSB Academic Planning',
@@ -12,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
